7c33e89b03364a12e825cb19c33ffad9083ceeb2
npx <harisudheer@lyrostech.com>
2024-05-08 11:47:22 +0530

    commit

---
+++ src/component/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id  : '1',
+      question: "What is a Mern Stack?",
+      answer:
+        "The MERN stack is a collection of technologies that help developers build robust and scalable web applications using JavaScript. The acronym “MERN” stands for MongoDB, Express, React, and Node.js, with each component playing a role in the development process.",
+    },
+    {
+      id  : '2',
+      question: "What are the technologies?",
+      answer:
+        "MongoDB is a highly scalable document database that makes it easy to store and retrieve data in JSON documents.Express is a lightweight web application framework that provides a range of app-building tools and supports a variety of programming languages, including JavaScript.React is an open source, front-end JavaScript library for building user interfaces based on components.Node.js is a runtime environment that can be used to run JavaScript code on the server side. This allows developers to use the same language for both the front and back ends of their applications.hey are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.",
+    },
+    {
+      id  : '3',
+      question: "Mern stack use cases",
+      answer:
+        " Real time application, SPAs, Data-driven applications.",
+    },
+    {
+      id  : '4',
+      question: "Can I create an accordion component with a different framework?",
+      answer:
+        "Yes of course, it is very possible to create an accordion component with another framework.",
+    },
+  ];
+
+  export default data;
